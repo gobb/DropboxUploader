042d88304661dae90040ee79d530682e7600503c
hakre <hanskrentel@yahoo.de>
2013-04-24 16:33:26 +0200

    Version bump v1.1.13

--- DropboxUploader.php
+++ DropboxUploader.php
@@ -23,7 +23,7 @@
  * THE SOFTWARE.
  *
  * @author Jaka Jancar [jaka@kubje.org] [http://jaka.kubje.org/]
- * @version 1.1.12
+ * @version 1.1.13
  * @lincense MIT <http://spdx.org/licenses/MIT>
  */
 final class DropboxUploader {
