49898066e19b6c501da87f199c6fc5463c7f9061
hakre <hanskrentel@yahoo.de>
2013-04-24 21:08:56 +0200

    Update: Test: Constructor test to cover parameter exceptions

--- test/unit/DropboxUploaderTest.php
+++ test/unit/DropboxUploaderTest.php
@@ -10,6 +10,31 @@ class DropboxUploaderTest extends BaseTestCase
     public function testConstructor() {
         $this->addToAssertionCount(1);
         $uploader = new DropboxUploader('email', 'pass');
+        $this->assertInstanceOf('DropboxUploader', $uploader);
+
+
+        $exception = false;
+        try {
+            $this->addToAssertionCount(1);
+            $uploader = new DropboxUploader('', '');
+        } catch (Exception $e) {
+            $code = $uploader::CODE_PARAMETER_TYPE_ERROR;
+            $this->assertSame($code, $e->getCode());
+            $exception = true;
+        }
+        $this->assertSame(true, $exception);
+
+        $exception = false;
+
+        try {
+            $this->addToAssertionCount(1);
+            $uploader = new DropboxUploader('email', '');
+        } catch (Exception $e) {
+            $code = $uploader::CODE_PARAMETER_TYPE_ERROR;
+            $this->assertSame($code, $e->getCode());
+            $exception = true;
+        }
+        $this->assertSame(true, $exception);
     }
 
     public function testSetters() {
